0bb0ec1b9cacf2fba3143a7dec92c6cd77287d96
Yu-Ren Liu <liuyr@lamda.nju.edu.cn>
2018-02-10 01:45:03 +0800

    zoosrv 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zoosrv',
-    version="0.1.2",
+    version="0.1.3",
     description=(
         'Python Servers for Distributed Zeroth-Order Optimization'
     ),
